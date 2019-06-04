c91af7426605459759aaa27679255a8e7b039c25
JakeGrath <J.A.McGrath@outlook.com>
2019-06-04 18:12:23 -0500

    Initial commit ._., meets yesterday's MVP? Today's WIP

---
+++ instagram-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './dummy-data'
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts : []
+    }
+  }
+
+componentDidMount () {
+  this.setState({
+    posts: dummyData
+  });
+}
+
+render () {
+  return (
+    <div className="App">
+      <SearchBar />
+      <PostContainer posts={this.state.posts} />
+    </div>
+  );
+}
+
+}
+export default App;
